0022d35b9df21113d4f79a425ca4d19b6edf2e0b
dbotti <davide.botti@lastminute.com>
2025-07-03 23:30:05 +0200

    Finish setup

--- webapp/src/main/kotlin/webapp/configuration/WebConfiguration.kt
+++ webapp/src/main/kotlin/webapp/configuration/WebConfiguration.kt
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfiguration : WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
-        // Forward requests to / to index.html
         registry.addViewController("/").setViewName("forward:/index.html")
     }
 }
