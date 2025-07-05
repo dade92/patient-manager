9fe6aaf83986067aee13db23b5dcd9d0f410ea9e
Davide Botti <davidebotti92@gmail.com>
2025-07-05 12:08:15 +0200

    Extract a content type resolver that can set the proper content type: this allows the browser to proper show the file without downloading it

--- webapp/src/main/kotlin/webapp/configuration/UtilsConfiguration.kt
+++ webapp/src/main/kotlin/webapp/configuration/UtilsConfiguration.kt
@@ -1,13 +1,17 @@
 package webapp.configuration
 
-import domain.utils.DefaultDateTimeProvider
 import domain.utils.DateTimeProvider
+import domain.utils.DefaultDateTimeProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import webapp.utils.ContentTypeResolver
 
 @Configuration
 class UtilsConfiguration {
 
     @Bean
     fun instantProvider(): DateTimeProvider = DefaultDateTimeProvider()
+
+    @Bean
+    fun contentTypeResolver(): ContentTypeResolver = ContentTypeResolver()
 }
