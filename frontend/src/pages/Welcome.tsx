ce4915b93275202c2672cd342cdaa8090687a8ca
Davide Botti <davidebotti92@gmail.com>
2025-07-05 15:49:19 +0200

    Extract the menu of the patient manager

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,25 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
-import {AppBar, Box, Container, IconButton, Menu, MenuItem, Toolbar, Typography} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import {AppBar, Box, Container, Toolbar, Typography} from '@mui/material';
 import {PatientSearch} from '../components/PatientSearch';
 import {PatientDetail} from './PatientDetail';
 import {NewPatient} from './NewPatient';
 import {OperationDetail} from './OperationDetail';
 import {CacheProvider} from '../context/CacheContext';
+import {PatientManagerMenu} from '../components/PatientManagerMenu';
 
 const Welcome: React.FC = () => {
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-
-    const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
     return (
         <CacheProvider>
             <BrowserRouter>
@@ -33,35 +22,7 @@ const Welcome: React.FC = () => {
                         >
                             Patient Manager
                         </Typography>
-                        <IconButton
-                            color="inherit"
-                            onClick={handleMenuClick}
-                            edge="end"
-                            aria-label="menu"
-                            aria-controls={open ? 'actions-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                        >
-                            <MenuIcon/>
-                        </IconButton>
-                        <Menu
-                            id="actions-menu"
-                            anchorEl={anchorEl}
-                            open={open}
-                            onClose={handleClose}
-                            MenuListProps={{
-                                'aria-labelledby': 'actions-button',
-                            }}
-                        >
-                            <MenuItem
-                                component={Link}
-                                to="/new-patient"
-                                onClick={handleClose}
-                            >
-                                New Patient
-                            </MenuItem>
-                            {/* Add more menu items here in the future */}
-                        </Menu>
+                        <PatientManagerMenu/>
                     </Toolbar>
                 </AppBar>
 
