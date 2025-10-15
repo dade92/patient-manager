de95dbc1169325c9fa7ea9c692f3ce1b9e80a07b
dbotti <davide.botti@lastminute.com>
2025-10-15 15:50:15 +0200

    Tests the content type resolver

--- webapp/src/test/kotlin/webapp/utils/ContentTypeResolverTest.kt
+++ webapp/src/test/kotlin/webapp/utils/ContentTypeResolverTest.kt
@@ -10,29 +10,22 @@ class ContentTypeResolverTest {
 
     @ParameterizedTest(name = "{index} => '{0}' -> {1}")
     @CsvSource(
-        // PDF
         "file.pdf, application/pdf",
         "FILE.PDF, application/pdf",
-        // JPEG
         "image.jpg, image/jpeg",
         "photo.JPEG, image/jpeg",
-        // PNG
         "icon.png, image/png",
-        // GIF
         "anim.gif, image/gif",
-        // SVG
         "vector.svg, image/svg+xml",
-        // TXT
         "readme.txt, text/plain",
-        // HTML/HTM
         "index.html, text/html",
         "index.htm, text/html",
-        // Default (unknown)
         "archive.zip, application/octet-stream",
         "noextension, application/octet-stream"
     )
     fun `getContentType resolves expected media type`(filename: String, expectedContentType: String) {
         val result = resolver.getContentType(filename)
+
         assertEquals(expectedContentType, result)
     }
 }
