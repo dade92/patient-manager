3266cec5c39737742d81beb89f936883c929d797
Davide Botti <davidebotti92@gmail.com>
2025-10-19 14:15:28 +0200

    Removes default parameters in Patient

--- domain/src/main/kotlin/domain/model/Patient.kt
+++ domain/src/main/kotlin/domain/model/Patient.kt
@@ -11,10 +11,10 @@ data class Patient(
     val id: PatientId,
     val name: String,
     val email: String,
-    val phoneNumber: String? = null,
-    val address: String? = null,
-    val cityOfResidence: String? = null,
-    val nationality: String? = null,
+    val phoneNumber: String?,
+    val address: String?,
+    val cityOfResidence: String?,
+    val nationality: String?,
     val birthDate: LocalDate,
     val taxCode: String
 )
