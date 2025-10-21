07fb7cb2d67cd144f04bdddd4f231cae93b3cbb4
dbotti <davide.botti@lastminute.com>
2025-10-21 20:41:27 +0200

    Adds correct numbering of teeth

--- frontend/src/utils/teethUtils.ts
+++ frontend/src/utils/teethUtils.ts
@@ -1,9 +1,9 @@
 export const generateFdiTeethNumbers = (): number[] => {
   return [
-    ...Array.from({ length: 8 }, (_, i) => 11 + i), // Quadrant 1: 11-18
-    ...Array.from({ length: 8 }, (_, i) => 21 + i), // Quadrant 2: 21-28
-    ...Array.from({ length: 8 }, (_, i) => 31 + i), // Quadrant 3: 31-38
-    ...Array.from({ length: 8 }, (_, i) => 41 + i), // Quadrant 4: 41-48
+    ...Array.from({ length: 8 }, (_, i) => 11 + i),
+    ...Array.from({ length: 8 }, (_, i) => 21 + i),
+    ...Array.from({ length: 8 }, (_, i) => 31 + i),
+    ...Array.from({ length: 8 }, (_, i) => 41 + i),
   ];
 };
 
