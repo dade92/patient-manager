c67f6bc8c8b71e31cd57def4ff96e11e70d7577d
dbotti <davide.botti@lastminute.com>
2025-10-22 15:18:20 +0200

    Adds validators of the create operation request

--- domain/src/test/kotlin/domain/operation/OperationServiceTest.kt
+++ domain/src/test/kotlin/domain/operation/OperationServiceTest.kt
@@ -11,14 +11,12 @@ import domain.model.OperationBuilder.anOperationId
 import domain.model.OperationType.SURGERY
 import domain.model.PatientBuilder.aPatient
 import domain.model.PatientBuilder.aPatientId
+import domain.operation.validator.OperationRequestValidator
 import domain.patient.PatientRepository
 import domain.storage.StorageService
 import domain.storage.UploadFileRequest
 import domain.utils.DateTimeProvider
-import io.mockk.Called
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
@@ -33,13 +31,15 @@ class OperationServiceTest {
     private val operationIdGenerator = mockk<OperationIdGenerator>()
     private val dateTimeProvider = mockk<DateTimeProvider>()
     private val storageService = mockk<StorageService>()
+    private val operationRequestValidator = mockk<OperationRequestValidator>()
 
     private val operationService = OperationService(
         patientRepository = patientRepository,
         operationRepository = operationRepository,
         operationIdGenerator = operationIdGenerator,
         dateTimeProvider = dateTimeProvider,
-        storageService = storageService
+        storageService = storageService,
+        operationRequestValidator = operationRequestValidator
     )
 
     @Test
@@ -65,6 +65,7 @@ class OperationServiceTest {
             info = PATIENT_OPERATION_INFO
         )
 
+        every { operationRequestValidator.validate(request) } just Runs
         every { patientRepository.retrieve(PATIENT_ID) } returns PATIENT
         every { operationIdGenerator.get() } returns OPERATION_ID
         every { dateTimeProvider.now() } returns NOW
@@ -81,6 +82,7 @@ class OperationServiceTest {
             patientId = PATIENT_ID,
         )
 
+        every { operationRequestValidator.validate(request) } just Runs
         every { patientRepository.retrieve(PATIENT_ID) } returns null
 
         assertThrows(PatientNotFoundException::class.java) {
