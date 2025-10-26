e4a935a267a98dbbcb516deb678ab064e14f425a
Davide Botti <davidebotti92@gmail.com>
2025-10-26 17:14:14 +0100

    Change teeth button ordering to reflect nomenclature

--- frontend/src/utils/teethUtils.ts
+++ frontend/src/utils/teethUtils.ts
@@ -1,21 +1,21 @@
 export const generateFdiTeethNumbers = (): number[][] => [
     [
-        ...Array.from({length: 8}, (_, i) => 18 - i), // 18 to 11
-        ...Array.from({length: 8}, (_, i) => 21 + i)  // 21 to 28
+        ...Array.from({length: 8}, (_, i) => 18 - i),
+        ...Array.from({length: 8}, (_, i) => 21 + i)
     ],
     [
-        ...Array.from({length: 8}, (_, i) => 48 - i), // 48 to 41
-        ...Array.from({length: 8}, (_, i) => 31 + i)  // 31 to 38
+        ...Array.from({length: 8}, (_, i) => 48 - i),
+        ...Array.from({length: 8}, (_, i) => 31 + i)
     ]
 ];
 
 export const generateFdiDeciduousTeethNumbers = (): number[][] => [
     [
-        ...Array.from({length: 5}, (_, i) => 55 - i), // 55 to 51
-        ...Array.from({length: 5}, (_, i) => 61 + i)  // 61 to 65
+        ...Array.from({length: 5}, (_, i) => 55 - i),
+        ...Array.from({length: 5}, (_, i) => 61 + i)
     ],
     [
-        ...Array.from({length: 5}, (_, i) => 85 - i), // 85 to 81
-        ...Array.from({length: 5}, (_, i) => 71 + i)  // 71 to 75
+        ...Array.from({length: 5}, (_, i) => 85 - i),
+        ...Array.from({length: 5}, (_, i) => 71 + i)
     ]
 ];
